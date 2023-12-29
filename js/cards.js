7a76c2412b8d3f511f88ee51642ad83a27e17c4a
giacomosx <giacomo.bartoli@me.com>
2023-12-29 19:53:30 +0100

    card function fixed

--- js/cards.js
+++ js/cards.js
@@ -1,25 +1,21 @@
 const cardSection = document.querySelector('.card-section-left');
 
 
-function createCard() {
-    globalData.planets.forEach(element => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.id = element.id;
+globalData.planets.forEach(element => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.id = element.id;
 
-        const image = document.createElement('img');
-        const title = document.createElement('h3');
-        const description = document.createElement('p');
-        
-        image.src = element.path;
-        title.innerText = element.title;
-        description.innerText = element.description;
+    const image = document.createElement('img');
+    const title = document.createElement('h3');
+    const description = document.createElement('p');
+    
+    image.src = element.path;
+    title.innerText = element.title;
+    description.innerText = element.description;
 
-        card.append(image, title, description)
+    card.append(image, title, description)
 
-        cardSection.append(card)
+    cardSection.append(card)
 
-    });
-}
-
-createCard()
+});
