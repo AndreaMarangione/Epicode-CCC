1d47450576c30982ad69b16ea05b91cb32aea6d3
giacomosx <giacomo.bartoli@me.com>
2023-12-29 16:06:44 +0100

    added planet card

---
+++ js/animation.js
@@ -0,0 +1,62 @@
+stopBtn.onclick = function() {
+    earth.style.animationName = 'none';
+    mars.style.animationName = 'none';
+    venus.style.animationName = 'none';
+    jupiter.style.animationName = 'none';
+
+    popUpEarth.style.opacity = 1;
+    popUpEarth.style.top= '30px'
+
+    popUpMars.style.opacity = 1;
+    popUpMars.style.top= '30px'
+
+    popUpVenus.style.opacity = 1;
+    popUpVenus.style.top= '30px'
+
+    popUpJupiter.style.opacity = 1;
+    popUpJupiter.style.top= '30px'
+}
+
+playBtn.onclick = function() {
+    popUpEarth.style.opacity = 0;
+    popUpMars.style.opacity = 0;
+    popUpVenus.style.opacity = 0;
+    popUpJupiter.style.opacity = 0;
+
+
+    earthCard.style.opacity = 0;    
+    marsCard.style.opacity = 0;    
+    earthCard.style.top = '500px';   
+    marsCard.style.top = '500px';   
+    venusCard.style.opacity = 0;    
+    jupiterCard.style.opacity = 0;    
+    venusCard.style.top = '500px';   
+    jupiterCard.style.top = '500px';   
+
+    setTimeout(function(){
+        earth.style.animationName = 'rotation';
+        mars.style.animationName = 'rotation';
+        venus.style.animationName = 'rotation';
+        jupiter.style.animationName = 'rotation';
+    }, 700)
+}
+
+popUpEarth.onclick = function(){
+    earthCard.style.opacity = 1;
+    earthCard.style.top = 0;
+}
+
+popUpMars.onclick = function(){
+    marsCard.style.opacity = 1;
+    marsCard.style.top = 0;
+}
+
+popUpVenus.onclick = function(){
+    venusCard.style.opacity = 1;
+    venusCard.style.top = 0;
+}
+
+popUpJupiter.onclick = function(){
+    jupiterCard.style.opacity = 1;
+    jupiterCard.style.top = 0;
+}
---
+++ js/golbaldata.js
@@ -0,0 +1,42 @@
+const playBtn = document.getElementById('play');
+const stopBtn = document.getElementById('stop');
+
+const earth = document.getElementById('earth');
+const mars = document.getElementById('mars');
+const venus = document.getElementById('venus');
+const jupiter = document.getElementById('jupiter');
+
+const popUpEarth = document.getElementById('descEarth');
+const popUpMars = document.getElementById('descMars');
+const popUpVenus = document.getElementById('descVenus');
+const popUpJupiter = document.getElementById('descJupiter');
+
+
+const globalData = {
+    planets: [
+        {
+            title : 'Il Pianeta Terra',
+            description: "La Terra è il nostro pianeta, ricco di varietà. Oceani estesi, montagne imponenti e una vasta gamma di creature lo caratterizzano. Le stagioni portano cambiamenti, dalla neve all'estate. Esplorare la Terra significa scoprire luoghi sorprendenti e creare connessioni con la diversità che offre. 🌍✨",
+            path: './assets/imgCard/planetEarth.jpeg', 
+            id: 'earthCard'
+        },
+        {
+            title : 'Il Pianeta Marte',
+            description: "Marte, il nostro vicino nel sistema solare, è un pianeta rosso e affascinante. Con paesaggi desertici, valli profonde e poli gelati, offre uno spettacolo unico. Le sue stagioni sono lunghe, e le tempeste di sabbia possono essere intense. Esplorare Marte è un'avventura scientifica in continua evoluzione, aprendo nuove prospettive sul nostro sistema solare. 🚀🔴",
+            path: './assets/imgCard/planetMars.jpeg',
+            id: 'marsCard'
+        },
+        {
+            title : 'Il Pianeta Venere',
+            description: "Venere, la 'gemella' rovente della Terra, è avvolta da un'atmosfera densa e calda. Il suo paesaggio è caratterizzato da pianure vulcaniche e montagne imponenti. Senza stagioni come le conosciamo, la sua superficie è costantemente avvolta in una fitta coperta nuvolosa. Esplorare Venere sfida la scienza, offrendo insight preziosi sulle dinamiche atmosferiche e geologiche. Un pianeta misterioso che continua a intrigare gli esploratori spaziali. 🪐🔥" ,
+            path: './assets/imgCard/planetVenus.jpeg',
+            id: 'venusCard'
+        },
+        {
+            title : 'Il Pianeta Giove',
+            description: "Giove, il gigante gassoso del nostro sistema solare, è uno spettacolo maestoso. Con vaste fasce nuvolose e la Grande Macchia Rossa, è un pianeta di dimensioni imponenti. Le sue lune e il potente campo magnetico aggiungono al suo fascino. Esplorare Giove è un'avventura astrale, offrendo uno sguardo su fenomeni unici nel sistema solare e aprendo porte a nuove scoperte cosmiche. 🪐🌌",
+            path: './assets/imgCard/planetJupiter.jpeg',
+            id: 'jupiterCard'
+        },
+    ]
+}
