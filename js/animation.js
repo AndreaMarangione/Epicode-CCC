c718920dea91274146566c1d4d660762eb0bf140
Andrea Marangione <marangione9@gmail.com>
2024-01-02 17:54:06 +0100

    Added all planets

--- js/animation.js
+++ js/animation.js
@@ -1,118 +1,66 @@
 closingCardBtn.addEventListener('click', closingAll);
 
 stopBtn.onclick = function() {
+    mercury.style.animationName = 'none';
+    venus.style.animationName = 'none';
     earth.style.animationName = 'none';
     mars.style.animationName = 'none';
-    venus.style.animationName = 'none';
     jupiter.style.animationName = 'none';
+    saturn.style.animationName = 'none';
+    uranus.style.animationName = 'none';
+    neptune.style.animationName = 'none';
 
     popUpSun.style.opacity = 1;
-    popUpSun.style.top= '52%'
+    popUpSun.style.top = '52%';
 
-    popUpEarth.style.opacity = 1;
-    popUpEarth.style.top= '30px'
-
-    popUpMars.style.opacity = 1;
-    popUpMars.style.top= '30px'
+    popUpMercury.style.opacity = 1;
+    popUpMercury.style.top = '30px';
 
     popUpVenus.style.opacity = 1;
-    popUpVenus.style.top= '30px'
+    popUpVenus.style.top = '30px';
+
+    popUpEarth.style.opacity = 1;
+    popUpEarth.style.top = '30px';
+
+    popUpMars.style.opacity = 1;
+    popUpMars.style.top = '30px';
 
     popUpJupiter.style.opacity = 1;
-    popUpJupiter.style.top= '30px'
+    popUpJupiter.style.top = '30px';
+
+    popUpSaturn.style.opacity = 1;
+    popUpSaturn.style.top = '30px';
+
+    popUpUranus.style.opacity = 1;
+    popUpUranus.style.top = '30px';
+
+    popUpNeptune.style.opacity = 1;
+    popUpNeptune.style.top = '30px';
 }
 
 playBtn.onclick = function() {
     popUpSun.style.opacity = 0;
+    popUpMercury.style.opacity = 0;
+    popUpVenus.style.opacity = 0;
     popUpEarth.style.opacity = 0;
     popUpMars.style.opacity = 0;
-    popUpVenus.style.opacity = 0;
     popUpJupiter.style.opacity = 0;
+    popUpSaturn.style.opacity = 0;
+    popUpUranus.style.opacity = 0;
+    popUpNeptune.style.opacity = 0;
 
     closingAll() 
 
     setTimeout(function(){
+        mercury.style.animationName = 'rotation';
+        venus.style.animationName = 'rotation';
         earth.style.animationName = 'rotation';
         mars.style.animationName = 'rotation';
-        venus.style.animationName = 'rotation';
         jupiter.style.animationName = 'rotation';
+        saturn.style.animationName = 'rotation';
+        uranus.style.animationName = 'rotation';
+        neptune.style.animationName = 'rotation';
     }, 700)
-}
-
-popUpEarth.onclick = function(){
-    getClosingCardBtn();
-
-    earthCard.style.opacity = 1;
-    earthCard.style.top = '10%';
-
-    sunCard.style.opacity = 0; 
-    sunCard.style.top = '500px';
-
-    marsCard.style.opacity = 0;    
-    marsCard.style.top = '500px';   
-    
-    venusCard.style.opacity = 0;    
-    venusCard.style.top = '500px';   
-    
-    jupiterCard.style.opacity = 0;    
-    jupiterCard.style.top = '500px'; 
-}
-
-popUpMars.onclick = function(){
-    getClosingCardBtn();
-
-    marsCard.style.opacity = 1;
-    marsCard.style.top = '10%';
-
-    sunCard.style.opacity = 0; 
-    sunCard.style.top = '500px';
-
-    earthCard.style.opacity = 0;    
-    earthCard.style.top = '500px'; 
-
-    venusCard.style.opacity = 0;    
-    venusCard.style.top = '500px';   
-    
-    jupiterCard.style.opacity = 0;    
-    jupiterCard.style.top = '500px';   
-}
-
-popUpVenus.onclick = function(){
-    getClosingCardBtn();
-
-    venusCard.style.opacity = 1;
-    venusCard.style.top = '10%';
-
-    sunCard.style.opacity = 0; 
-    sunCard.style.top = '500px';
-
-    earthCard.style.opacity = 0;    
-    earthCard.style.top = '500px'; 
-
-    marsCard.style.opacity = 0;    
-    marsCard.style.top = '500px';   
-    
-    jupiterCard.style.opacity = 0;    
-    jupiterCard.style.top = '500px';   
-}
-
-popUpJupiter.onclick = function(){
-    getClosingCardBtn();
-
-    sunCard.style.opacity = 0;    
-    sunCard.style.top = '500px'; 
-    
-    jupiterCard.style.opacity = 1;
-    jupiterCard.style.top = '10%';
-
-    earthCard.style.opacity = 0;    
-    earthCard.style.top = '500px'; 
-
-    marsCard.style.opacity = 0;    
-    marsCard.style.top = '500px';   
-    
-    venusCard.style.opacity = 0;    
-    venusCard.style.top = '500px';   
 }
 
 popUpSun.onclick = function(){
@@ -121,15 +69,272 @@ popUpSun.onclick = function(){
     sunCard.style.opacity = 1;
     sunCard.style.top = '10%';
 
+    venusCard.style.opacity = 0;    
+    venusCard.style.top = '500px';
+
+    earthCard.style.opacity = 0;    
+    earthCard.style.top = '500px'; 
+
+    marsCard.style.opacity = 0;    
+    marsCard.style.top = '500px';   
+        
+    jupiterCard.style.opacity = 0;    
+    jupiterCard.style.top = '500px';   
+
+    saturnCard.style.opacity = 0;
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 0;
+    uranusCard.style.top = '500px'; 
+
+    neptuneCard.style.opacity = 0;
+    neptuneCard.style.top = '500px'; 
+}
+
+popUpMercury.onclick = function(){
+    getClosingCardBtn();
+
+    sunCard.style.opacity = 0; 
+    sunCard.style.top = '500px';
+
+    mercuryCard.style.opacity = 1;
+    mercuryCard.style.top = '10%';
+
+    venusCard.style.opacity = 0;
+    venusCard.style.top = '500px';
+
     earthCard.style.opacity = 0;    
     earthCard.style.top = '500px'; 
 
     marsCard.style.opacity = 0;    
     marsCard.style.top = '500px';   
     
-    venusCard.style.opacity = 0;    
-    venusCard.style.top = '500px';
+    jupiterCard.style.opacity = 0;    
+    jupiterCard.style.top = '500px';   
+
+    saturnCard.style.opacity = 0;
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 0;
+    uranusCard.style.top = '500px'; 
+    
+    neptuneCard.style.opacity = 0;
+    neptuneCard.style.top = '500px'; 
+}
+
+popUpVenus.onclick = function(){
+    getClosingCardBtn();
+
+    sunCard.style.opacity = 0; 
+    sunCard.style.top = '500px';
+
+    mercuryCard.style.opacity = 0;
+    mercuryCard.style.top = '500px';
+
+    venusCard.style.opacity = 1;
+    venusCard.style.top = '10%';
+
+    earthCard.style.opacity = 0;    
+    earthCard.style.top = '500px'; 
+
+    marsCard.style.opacity = 0;    
+    marsCard.style.top = '500px';   
     
     jupiterCard.style.opacity = 0;    
     jupiterCard.style.top = '500px';   
+
+    saturnCard.style.opacity = 0;
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 0;
+    uranusCard.style.top = '500px'; 
+
+    neptuneCard.style.opacity = 0;
+    neptuneCard.style.top = '500px'; 
+}
+
+popUpEarth.onclick = function(){
+    getClosingCardBtn();
+
+    sunCard.style.opacity = 0; 
+    sunCard.style.top = '500px';
+
+    mercuryCard.style.opacity = 0;
+    mercuryCard.style.top = '500px';
+
+    venusCard.style.opacity = 0;    
+    venusCard.style.top = '500px'; 
+
+    earthCard.style.opacity = 1;
+    earthCard.style.top = '10%';
+
+    marsCard.style.opacity = 0;    
+    marsCard.style.top = '500px';   
+        
+    jupiterCard.style.opacity = 0;    
+    jupiterCard.style.top = '500px'; 
+
+    saturnCard.style.opacity = 0;
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 0;
+    uranusCard.style.top = '500px'; 
+
+    neptuneCard.style.opacity = 0;
+    neptuneCard.style.top = '500px'; 
+}
+
+popUpMars.onclick = function(){
+    getClosingCardBtn();
+
+    sunCard.style.opacity = 0; 
+    sunCard.style.top = '500px';
+
+    mercuryCard.style.opacity = 0;
+    mercuryCard.style.top = '500px';
+
+    venusCard.style.opacity = 0;    
+    venusCard.style.top = '500px';  
+
+    earthCard.style.opacity = 0;    
+    earthCard.style.top = '500px'; 
+
+    marsCard.style.opacity = 1;
+    marsCard.style.top = '10%';
+    
+    jupiterCard.style.opacity = 0;    
+    jupiterCard.style.top = '500px';   
+
+    saturnCard.style.opacity = 0;
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 0;
+    uranusCard.style.top = '500px'; 
+
+    neptuneCard.style.opacity = 0;
+    neptuneCard.style.top = '500px'; 
+}
+
+popUpJupiter.onclick = function(){
+    getClosingCardBtn();
+
+    sunCard.style.opacity = 0;    
+    sunCard.style.top = '500px'; 
+    
+    mercuryCard.style.opacity = 0;
+    mercuryCard.style.top = '500px';
+
+    venusCard.style.opacity = 0;    
+    venusCard.style.top = '500px'; 
+
+    earthCard.style.opacity = 0;    
+    earthCard.style.top = '500px'; 
+
+    marsCard.style.opacity = 0;    
+    marsCard.style.top = '500px';   
+    
+    jupiterCard.style.opacity = 1;
+    jupiterCard.style.top = '10%'; 
+
+    saturnCard.style.opacity = 0;
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 0;
+    uranusCard.style.top = '500px'; 
+
+    neptuneCard.style.opacity = 0;
+    neptuneCard.style.top = '500px'; 
+}
+
+popUpSaturn.onclick = function(){
+    getClosingCardBtn();
+
+    sunCard.style.opacity = 0;    
+    sunCard.style.top = '500px'; 
+    
+    mercuryCard.style.opacity = 0;
+    mercuryCard.style.top = '500px';
+    
+    venusCard.style.opacity = 0;    
+    venusCard.style.top = '500px'; 
+
+    earthCard.style.opacity = 0;    
+    earthCard.style.top = '500px'; 
+
+    marsCard.style.opacity = 0;    
+    marsCard.style.top = '500px';   
+    
+    jupiterCard.style.opacity = 0;
+    jupiterCard.style.top = '500px';
+    
+    saturnCard.style.opacity = 1;
+    saturnCard.style.top = '10%'; 
+
+    uranusCard.style.opacity = 0;
+    uranusCard.style.top = '500px'; 
+
+    neptuneCard.style.opacity = 0;
+    neptuneCard.style.top = '500px'; 
+}
+
+popUpUranus.onclick = function(){
+    getClosingCardBtn();
+
+    sunCard.style.opacity = 0;    
+    sunCard.style.top = '500px'; 
+    
+    mercuryCard.style.opacity = 0;
+    mercuryCard.style.top = '500px';
+    
+    venusCard.style.opacity = 0;    
+    venusCard.style.top = '500px'; 
+
+    earthCard.style.opacity = 0;    
+    earthCard.style.top = '500px'; 
+
+    marsCard.style.opacity = 0;    
+    marsCard.style.top = '500px';   
+    
+    jupiterCard.style.opacity = 0;
+    jupiterCard.style.top = '500px';
+    
+    saturnCard.style.opacity = 0;
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 1;
+    uranusCard.style.top = '10%'; 
+
+    neptuneCard.style.opacity = 0;
+    neptuneCard.style.top = '500px'; 
+}
+
+popUpNeptune.onclick = function(){
+    getClosingCardBtn();
+
+    sunCard.style.opacity = 0;    
+    sunCard.style.top = '500px'; 
+    
+    mercuryCard.style.opacity = 0;
+    mercuryCard.style.top = '500px';
+    
+    venusCard.style.opacity = 0;    
+    venusCard.style.top = '500px'; 
+
+    earthCard.style.opacity = 0;    
+    earthCard.style.top = '500px'; 
+
+    marsCard.style.opacity = 0;    
+    marsCard.style.top = '500px';   
+    
+    jupiterCard.style.opacity = 0;
+    jupiterCard.style.top = '500px';
+    
+    saturnCard.style.opacity = 0;
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 0;
+    uranusCard.style.top = '500px'; 
+
+    neptuneCard.style.opacity = 1;
+    neptuneCard.style.top = '10%'; 
 }
--- js/golbaldata.js
+++ js/golbaldata.js
@@ -1,16 +1,24 @@
 const playBtn = document.getElementById('play');
 const stopBtn = document.getElementById('stop');
 
+const mercury = document.getElementById('mercury');
+const venus = document.getElementById('venus');
 const earth = document.getElementById('earth');
 const mars = document.getElementById('mars');
-const venus = document.getElementById('venus');
 const jupiter = document.getElementById('jupiter');
+const saturn = document.getElementById('saturn');
+const uranus = document.getElementById('uranus');
+const neptune = document.getElementById('neptune');
 
 const popUpSun = document.getElementById('descSun');
+const popUpMercury = document.getElementById('descMercury');
+const popUpVenus = document.getElementById('descVenus');
 const popUpEarth = document.getElementById('descEarth');
 const popUpMars = document.getElementById('descMars');
-const popUpVenus = document.getElementById('descVenus');
 const popUpJupiter = document.getElementById('descJupiter');
+const popUpSaturn = document.getElementById('descSaturn');
+const popUpUranus = document.getElementById('descUranus');
+const popUpNeptune = document.getElementById('descNeptune');
 
 const closingCardBtn = document.getElementById('closingCardBtn');
 
@@ -30,24 +38,32 @@ function closingAll(event) {
     sunCard.style.opacity = 0; 
     sunCard.style.top = '500px';
 
+    venusCard.style.opacity = 0;    
+    venusCard.style.top = '500px';   
+
     earthCard.style.opacity = 0;    
     earthCard.style.top = '500px'; 
 
     marsCard.style.opacity = 0;    
     marsCard.style.top = '500px';   
-    
-    venusCard.style.opacity = 0;    
-    venusCard.style.top = '500px';   
-    
+        
     jupiterCard.style.opacity = 0;    
     jupiterCard.style.top = '500px'; 
+
+    saturnCard.style.opacity = 0;    
+    saturnCard.style.top = '500px'; 
+
+    uranusCard.style.opacity = 0;    
+    uranusCard.style.top = '500px'; 
+
+    neptuneCard.style.opacity = 0;    
+    neptuneCard.style.top = '500px'; 
 }
 
 function getClosingCardBtn(event) {
     closingCardBtn.style.right = '0px';
     closingCardBtn.style.opacity = 1;
 }
-
 
 const globalData = {
     planets: [
@@ -56,6 +72,18 @@ const globalData = {
             description: "Il Sole, fulcro della nostra galassia, è una sorgente inesauribile di energia e luce. Una sfera ardente di gas caldo, il Sole irradia calore e luce, permettendo la vita sulla Terra. Le immense eruzioni solari e le danze di particelle creano uno spettacolo celeste. Esplorare il Sole è un viaggio nell'energia stellare, fornendo chiavi per comprendere il funzionamento fondamentale del nostro sistema solare. ☀️🌌",
             path: './assets/imgCard/starSun.jpeg', 
             id: 'sunCard'
+        },
+        {
+            title : 'Il Pianeta Mercurio',
+            description: "Da aggiungere" ,
+            path: './assets/imgCard/planetVenus.jpeg',
+            id: 'mercuryCard'
+        },
+        {
+            title : 'Il Pianeta Venere',
+            description: "Venere, la 'gemella' rovente della Terra, è avvolta da un'atmosfera densa e calda. Il suo paesaggio è caratterizzato da pianure vulcaniche e montagne imponenti. Senza stagioni come le conosciamo, la sua superficie è costantemente avvolta in una fitta coperta nuvolosa. Esplorare Venere sfida la scienza, offrendo insight preziosi sulle dinamiche atmosferiche e geologiche. Un pianeta misterioso che continua a intrigare gli esploratori spaziali. 🪐🔥" ,
+            path: './assets/imgCard/planetVenus.jpeg',
+            id: 'venusCard'
         },
         {
             title : 'Il Pianeta Terra',
@@ -70,17 +98,28 @@ const globalData = {
             id: 'marsCard'
         },
         {
-            title : 'Il Pianeta Venere',
-            description: "Venere, la 'gemella' rovente della Terra, è avvolta da un'atmosfera densa e calda. Il suo paesaggio è caratterizzato da pianure vulcaniche e montagne imponenti. Senza stagioni come le conosciamo, la sua superficie è costantemente avvolta in una fitta coperta nuvolosa. Esplorare Venere sfida la scienza, offrendo insight preziosi sulle dinamiche atmosferiche e geologiche. Un pianeta misterioso che continua a intrigare gli esploratori spaziali. 🪐🔥" ,
-            path: './assets/imgCard/planetVenus.jpeg',
-            id: 'venusCard'
-        },
-        {
             title : 'Il Pianeta Giove',
             description: "Giove, il gigante gassoso del nostro sistema solare, è uno spettacolo maestoso. Con vaste fasce nuvolose e la Grande Macchia Rossa, è un pianeta di dimensioni imponenti. Le sue lune e il potente campo magnetico aggiungono al suo fascino. Esplorare Giove è un'avventura astrale, offrendo uno sguardo su fenomeni unici nel sistema solare e aprendo porte a nuove scoperte cosmiche. 🪐🌌",
             path: './assets/imgCard/planetJupiter.jpeg',
             id: 'jupiterCard'
         },
+        {
+            title : 'Il Pianeta Saturno',
+            description: "Da aggiungere",
+            path: './assets/imgCard/planetJupiter.jpeg',
+            id: 'saturnCard'
+        },
+        {
+            title : 'Il Pianeta Urano',
+            description: "Da aggiungere",
+            path: './assets/imgCard/planetJupiter.jpeg',
+            id: 'uranusCard'
+        },
+        {
+            title : 'Il Pianeta Nettuno',
+            description: "Da aggiungere",
+            path: './assets/imgCard/planetJupiter.jpeg',
+            id: 'neptuneCard'
+        },
     ]
 }
-
